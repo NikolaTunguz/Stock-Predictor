0791aa762c1878274b210f43cad3a7bbc9a39a62
Joel Molina <nvmolinajoel@gmail.com>
2024-11-13 20:30:22 -0800

    complete neural network

--- Models/data.py
+++ Models/data.py
@@ -4,23 +4,20 @@ class DataPreprocessing():
     def __init__(self, file):
         self.data = None
         self.file = file
+        self.read_data()
 
     def read_data(self):
         self.data = pd.read_csv(self.file)
-        return self.data
 
     def preprocessing(self):
         self.data = self.data.drop(columns=["Dividends", "Stock Splits"])
-        print(self.data.head())
         
         #create target columns which are the current columns shifted up 1.
-        self.data["tommorow_open"] = self.data["Open"].shift(-1)
-        self.data["tommorow_high"] = self.data["High"].shift(-1)
-        self.data["tommorow_low"] = self.data["Low"].shift(-1)
-        self.data["tommorow_close"] = self.data["Close"].shift(-1)
+        self.data["tomorrow_open"] = self.data["Open"].shift(-1)
+        self.data["tomorrow_high"] = self.data["High"].shift(-1)
+        self.data["tomorrow_low"] = self.data["Low"].shift(-1)
+        self.data["tomorrow_close"] = self.data["Close"].shift(-1)
         
-        
-
         #drop rows with a null value in any column.
         self.data = self.data.dropna()
 
--- Models/neural_network.py
+++ Models/neural_network.py
@@ -1,76 +1,79 @@
+#nn imports
 import torch
 import torch.nn as nn
 from torch.utils.data import DataLoader, TensorDataset
 
-import sklearn
+#data imports
 from sklearn.model_selection import train_test_split
-from sklearn.preprocessing import StandardScaler
-from sklearn.metrics import accuracy_score
 import data 
+import numpy as np
+import matplotlib.pyplot as plt
 
 class NeuralNetwork(nn.Module):
+    #define global variables and model
     def __init__(self, input_dim, output_dim):
         super(NeuralNetwork, self).__init__()
-
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.epoch_train_loss = []
-        self.epoch_train_acc = []
         self.epoch_val_loss = []
-        self.epoch_val_acc = []
         self.num_epochs = None
+
         hidden_size1 = 256
         hidden_size2 = 128
 
         self.fc1 = nn.Linear(input_dim, hidden_size1)
         self.activation1 = nn.ReLU()
+
         self.fc2 = nn.Linear(hidden_size1, hidden_size2)
         self.activation2 = nn.ReLU()
+        self.drop = nn.Dropout(0.5)
+
         self.fc3 = nn.Linear(hidden_size2, output_dim)
 
+    #forward pass through model
     def forward(self, x):
         output = self.fc1(x)
         output = self.activation1(output)
+
         output = self.fc2(output)
         output = self.activation2(output)
+        output = self.drop(output)
+
         output = self.fc3(output)
         return output
         
     def prepare_data(self, file):
         #set up generic CSV
         data_processing = data.DataPreprocessing(file)
-        dataset = data_processing.read_data()
         dataset = data_processing.preprocessing()
 
         #define features and targets
-        features = dataset[["Open", "High", "Low", "Close"]]
-        targets = dataset[["tommorow_open", "tommorow_high", "tommorow_low", "tommorow_close"]]
+        features = dataset[["Open", "High", "Low", "Close"]].values #values on a day
+        targets = dataset[["tomorrow_open", "tomorrow_high", "tomorrow_low", "tomorrow_close"]].values #"next day" values
 
         #split dataset
-        self.x_train, self.y_val, self.y_train, self.y_val = train_test_split(features, targets, test_size=0.2, random_state=42)
+        #not shuffled so that the data maintains chronological order
+        self.x_train, self.x_val, self.y_train, self.y_val = train_test_split(features, targets, test_size=0.2, random_state=42, shuffle=False) 
+
+        #convert to tensors
         self.x_train = torch.tensor(self.x_train, dtype=torch.float32)
-        self.y_train = torch.tensor(self.x_train, dtype=torch.float32)
+        self.y_train = torch.tensor(self.y_train, dtype=torch.float32)
         self.x_val = torch.tensor(self.x_val, dtype=torch.float32)
         self.y_val = torch.tensor(self.y_val, dtype=torch.float32)
-        
-        #normalize data
-        normalize = StandardScaler()
-        self.x_train = normalize.fit_transform(self.x_train)
-        self.x_val = normalize.transform(self.x_val)
 
         #place datasets in dataloader
         train_dataset = TensorDataset(self.x_train, self.y_train)
         val_dataset = TensorDataset(self.x_val, self.y_val)
         batch_size = 64
-        self.train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=True)
+        self.train_loader = DataLoader(train_dataset, batch_size=batch_size, shuffle=False)
         self.val_loader = DataLoader(val_dataset, batch_size=batch_size, shuffle=False)
 
     def train_model(self, optimizer, criterion, num_epochs):
+        self.num_epochs = num_epochs
         for epoch in range(num_epochs):
             self.train()
             batch_train_loss = []
-            batch_train_acc = []
             batch_val_loss = []
-            batch_val_acc = []
 
             #training
             for(features, targets) in self.train_loader:
@@ -78,24 +81,14 @@ class NeuralNetwork(nn.Module):
 
                 optimizer.zero_grad()
                 predictions = self(features)
-
                 loss = criterion(predictions, targets)
                 batch_train_loss.append(loss.item())
-
                 loss.backward()
                 optimizer.step()
 
-                #batch accuracy
-                _, prediction_labels = torch.max(predictions, dim=1)
-                targets = targets.cpu().numpy()
-                prediction_labels = prediction_labels.cpu().numpy()
-                batch_train_acc.append(accuracy_score(prediction_labels, labels))
-
-            #epoch accuracy and loss
-            train_acc = sum(batch_train_acc) / len(batch_train_acc)
-            self.epoch_train_acc.append(train_acc)
+            #epoch training loss
             train_loss = sum(batch_train_loss) / len(batch_train_loss)
-            self.epoch_val_loss.append(train_loss)
+            self.epoch_train_loss.append(train_loss)
 
             #validation
             self.eval()
@@ -104,39 +97,62 @@ class NeuralNetwork(nn.Module):
                     features,targets = features.to(self.device), targets.to(self.device)
 
                     predictions = self(features)
-
                     loss = criterion(predictions, targets)
                     batch_val_loss.append(loss.item())
 
-                    #batch accuracy
-                    _, prediction_labels = torch.max(predictions, dim=1)
-                    targets = targets.cpu().numpy()
-                    prediction_labels = prediction_labels.cpu().numpy()
-                    batch_val_acc.append(accuracy_score(prediction_labels, targets))
-            
-            #epoch accuracy and loss
-            val_acc = sum(batch_val_acc) / len(batch_val_acc)
-            self.epoch_val_acc.append(val_acc)
+            #epoch validation loss
             val_loss = sum(batch_val_loss) / len(batch_val_loss)
             self.epoch_val_loss.append(val_loss)
 
-        print(f"Epoch: {epoch+1} train acc: {self.epoch_train_acc[epoch]:.4f} train loss: {self.epoch_train_loss[epoch]:.4f} val acc: {self.epoch_val_acc[epoch]:.4f} val loss: {self.epoch_val_loss[epoch]:.4f}")
+            print(f"Epoch: {epoch+1} train loss: {self.epoch_train_loss[epoch]:.4f} val loss: {self.epoch_val_loss[epoch]:.4f}")
+    
+    #graph training and validation loss
+    def graph(self):
+        plt.figure(figsize=(12, 6))
+        plt.title("Loss")
+        plt.xlabel("Epoch")
+        plt.ylabel("Loss")
+        epochs = range(1, self.num_epochs+1)
+        plt.plot(epochs, self.epoch_train_loss, label="Training Loss")
+        plt.plot(epochs, self.epoch_val_loss, label="Validation Loss")
+        plt.legend()
+        plt.show()
 
+    #next day prediction
+    def predict(self, most_recent_day):
+        self.eval()
+        with torch.no_grad():
+            most_recent_day = most_recent_day.to(self.device)
+            
+            #forward pass on the most recent day
+            prediction = self(most_recent_day)
+            return prediction.cpu().numpy().flatten()
+        
 def main():
     #4 input features 4 target features
     model = NeuralNetwork(4, 4)
     device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
     model = model.to(device)
 
-    criterion = nn.CrossEntropyLoss()
-    optimizer = torch.optim.Adam(model.parameters(), lr=0.001)
-    num_epochs = 5
-
-    file = "data.csv"
+    #hyperparameters
+    criterion = nn.MSELoss()
+    optimizer = torch.optim.Adam(model.parameters(), lr=0.0001)
+    num_epochs = 20
 
     #train model
+    file = "data.csv"
     model.prepare_data(file)
     model.train_model(optimizer, criterion, num_epochs)
+
+    #training graph
+    model.graph()
+
+    #prediction based on last day
+    most_recent_day = model.x_val[-1]
+    tomorrow_prices = model.predict(most_recent_day)
+    
+    # Print the predicted prices
+    print("Tommorow's Predicted Prices (Open  High  Low  Close):", tomorrow_prices[0], " | ", tomorrow_prices[1], " | ", tomorrow_prices[2], " | ", tomorrow_prices[3])
 
 if __name__ == "__main__":
     main()
