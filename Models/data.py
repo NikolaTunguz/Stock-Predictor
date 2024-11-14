71b1ad5e9bafed6dee0639c4d6d68f2babe567e5
Joel Molina <nvmolinajoel@gmail.com>
2024-11-13 18:16:58 -0800

    neural_network
    not complete

---
+++ Models/data.py
@@ -0,0 +1,26 @@
+import pandas as pd
+
+class DataPreprocessing():
+    def __init__(self, file):
+        self.data = None
+        self.file = file
+
+    def read_data(self):
+        self.data = pd.read_csv(self.file)
+
+    def preprocessing(self):
+        self.data = self.data.drop(columns=["Dividends", "Stock Splits"])
+        print(self.data.head())
+        
+        #create target columns which are the current columns shifted up 1.
+        self.data["tommorow_open"] = self.data["Open"].shift(-1)
+        self.data["tommorow_high"] = self.data["High"].shift(-1)
+        self.data["tommorow_low"] = self.data["Low"].shift(-1)
+        self.data["tommorow_close"] = self.data["Close"].shift(-1)
+        
+        
+
+        #drop rows with a null value in any column.
+        self.data = self.data.dropna()
+
+
