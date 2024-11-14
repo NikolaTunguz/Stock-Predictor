8c7af40b984eb4316c2a1edb6eec62ee75fb0b77
Nikola Tunguz <tunguznikola@gmail.com>
2024-11-13 20:28:35 -0800

    adding comments to linear regression

--- Models/linear_regression.py
+++ Models/linear_regression.py
@@ -4,10 +4,12 @@ from sklearn.metrics import mean_absolute_percentage_error
 
 class LinearRegression:
     def __init__(self, X, y):
+        #core model variables
         self.model = LinearRegression()
         self.X = X
         self.y = y
 
+        #model varables for train/test split
         self.X_train = None
         self.y_train = None
         self.X_test = None
@@ -15,16 +17,20 @@ class LinearRegression:
 
         self.split_data()
     
+    #function to split data into training and test
     def split_data(self):
         self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(self.X, self.y, test_size = 0.2, random_state = 42)
 
+    #function to train the model
     def train(self):
         self.model.fit(self.X_train, self.y_train)
 
+    #private function to predict on test dataset
     def predict(self):
         prediction = self.model.predict(self.X_test)
         return prediction
 
+    #function to evaluate prediction performance
     def evaluate(self):
         prediction = self.predict()
         accuracy = mean_absolute_percentage_error(self.y_test, prediction)
