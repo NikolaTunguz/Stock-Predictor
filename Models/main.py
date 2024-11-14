3d7221239b5b2ef15acc25d13051b72e21740a6c
Nikola Tunguz <tunguznikola@gmail.com>
2024-11-13 20:29:19 -0800

    updating main.py
    adding the use of linear regression and comments

--- Models/main.py
+++ Models/main.py
@@ -11,11 +11,19 @@ def main():
     data_preprocessor = data.DataPreprocessor(file_name)
     dataset = data_preprocessor.preprocessing()
 
-    #call models
-    linear_regression = None
+    #extracting x and y data
+    labels = dataset["tomorrow_open", "tomorrow_high", "tomorrow_low", "tomorrow_close"]
+    features = dataset.drop(columns=["tomorrow_open", "tomorrow_high", "tomorrow_low", "tomorrow_close"])
+
+    #declare models, passing in x and y data
+    linear_regression = linear_regression.LinearRegression(features, labels)
     neural_network = None
 
-    
+    #train and evaluate models 
+    linear_regression.train()
+    linear_accuracy = linear_regression.evaluate()
+
+    print(linear_accuracy)
 
 if __name__ == "__main__":
     main()
