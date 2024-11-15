200df3dabc5d5518ddfc172f1bd712bd855133ea
Nikola Tunguz <tunguznikola@gmail.com>
2024-11-14 17:32:06 -0800

    updating main.py to include xgboost

--- Models/main.py
+++ Models/main.py
@@ -2,8 +2,10 @@
 import data
 
 #model imports
-import linear_regression
+import linear_regression_class
+import xgboost_class
 import neural_network
+
 
 def main():
     #process/update data with new columns
@@ -16,14 +18,22 @@ def main():
     features = dataset.drop(columns = ["Date", "tomorrow_open", "tomorrow_high", "tomorrow_low", "tomorrow_close"])
 
     #declare models, passing in x and y data
-    linear_regression_model = linear_regression.MyLinearRegression(features, labels)
+    linear_regression_model = linear_regression_class.MyLinearRegression(features, labels)
+    xgboost_model = xgboost_class.MyXGBoost(features, labels)
+
     neural_network = None
 
     #train and evaluate models 
     linear_regression_model.train()
     linear_error = linear_regression_model.evaluate()
 
+    xgboost_model.train()
+    xgboost_error = xgboost_model.evaluate()
+
+
+
     print("Linear Test Dataset Percent Error: ", linear_error * 100)
+    print("XGBoost Test Dataset Percent Error: ", xgboost_error * 100)
 
 if __name__ == "__main__":
     main()
